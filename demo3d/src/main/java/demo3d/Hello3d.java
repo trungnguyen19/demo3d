f0c87a82e8c5aa140d9df935ed72c90b386ab98a
trungnguyen19 <nongdan2m@gmail.com>
2014-05-10 13:16:13 +0700

    demo

---
+++ demo3d/src/main/java/demo3d/Hello3d.java
@@ -0,0 +1,36 @@
+package demo3d;
+
+import com.sun.j3d.utils.universe.SimpleUniverse;
+
+import com.sun.j3d.utils.geometry.ColorCube;
+
+import javax.media.j3d.BranchGroup;
+
+public class Hello3d {
+
+	public Hello3d()
+
+	{
+
+		SimpleUniverse universe = new SimpleUniverse();
+
+		BranchGroup group = new BranchGroup();
+
+		group.addChild(new ColorCube(0.3));
+
+		universe.getViewingPlatform().setNominalViewingTransform();
+
+		universe.addBranchGraph(group);
+
+	}
+
+	public static void main(String[] args) {
+
+		System.setProperty( "java.library.path", "/data/os/java-3d/lib" );
+		System.out.println("LD Library Path:"
+				+ System.getProperty("java.library.path"));
+		new Hello3d();
+
+	}
+
+} // end of class Hello3d
