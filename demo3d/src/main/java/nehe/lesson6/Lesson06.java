387530dfcfa24c7720bc2011018f224539907235
trungnguyen19 <nongdan2m@gmail.com>
2014-05-11 21:39:21 +0700

    Fix and add more lesson

---
+++ demo3d/src/main/java/nehe/lesson6/Lesson06.java
@@ -0,0 +1,391 @@
+package nehe.lesson6;
+
+/*
+ * Lesson06.java
+ *
+ * Created on July 16, 2003, 11:30 AM
+ */
+
+import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferInt;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import javax.imageio.ImageIO;
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.GLException;
+import javax.media.opengl.glu.GLU;
+
+import com.sun.opengl.util.Animator;
+import com.sun.opengl.util.texture.Texture;
+import com.sun.opengl.util.texture.TextureIO;
+
+//import net.java.games.jogl.*;
+//import net.java.games.jogl.util.*;
+
+/**
+ * Port of the NeHe OpenGL Tutorial (Lesson 6) to Java using the Jogl interface
+ * to OpenGL. Jogl can be obtained at http://jogl.dev.java.net/
+ * 
+ * @author Kevin Duling (jattier@hotmail.com)
+ */
+public class Lesson06 {
+	static Animator animator = null;
+
+	static class Renderer implements GLEventListener, KeyListener {
+		private float xrot; // X Rotation ( NEW )
+		private float yrot; // Y Rotation ( NEW )
+		private float zrot; // Z Rotation ( NEW )
+		private int texture;
+
+		private GLU glu; // for the GL Utility
+
+		private Texture myTexture;
+
+		/**
+		 * Called by the drawable to initiate OpenGL rendering by the client.
+		 * After all GLEventListeners have been notified of a display event, the
+		 * drawable will swap its buffers if necessary.
+		 * 
+		 * @param gLDrawable
+		 *            The GLDrawable object.
+		 */
+		public void display(GLAutoDrawable gLDrawable) {
+			final GL gl = gLDrawable.getGL();
+
+			myTexture.enable();
+			myTexture.bind();
+
+			gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+			gl.glLoadIdentity(); // Reset The View
+			gl.glTranslatef(0.0f, 0.0f, -5.0f);
+
+			gl.glRotatef(xrot, 1.0f, 0.0f, 0.0f);
+			gl.glRotatef(yrot, 0.0f, 1.0f, 0.0f);
+			gl.glRotatef(zrot, 0.0f, 0.0f, 1.0f);
+
+			// gl.glBindTexture(GL.GL_TEXTURE_2D, texture);
+
+			gl.glBegin(GL.GL_QUADS);
+			// Front Face
+			gl.glTexCoord2f(0.0f, 0.0f);
+			gl.glVertex3f(-1.0f, -1.0f, 1.0f);
+			gl.glTexCoord2f(1.0f, 0.0f);
+			gl.glVertex3f(1.0f, -1.0f, 1.0f);
+			gl.glTexCoord2f(1.0f, 1.0f);
+			gl.glVertex3f(1.0f, 1.0f, 1.0f);
+			gl.glTexCoord2f(0.0f, 1.0f);
+			gl.glVertex3f(-1.0f, 1.0f, 1.0f);
+			// Back Face
+			gl.glTexCoord2f(1.0f, 0.0f);
+			gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+			gl.glTexCoord2f(1.0f, 1.0f);
+			gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+			gl.glTexCoord2f(0.0f, 1.0f);
+			gl.glVertex3f(1.0f, 1.0f, -1.0f);
+			gl.glTexCoord2f(0.0f, 0.0f);
+			gl.glVertex3f(1.0f, -1.0f, -1.0f);
+			// Top Face
+			gl.glTexCoord2f(0.0f, 1.0f);
+			gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+			gl.glTexCoord2f(0.0f, 0.0f);
+			gl.glVertex3f(-1.0f, 1.0f, 1.0f);
+			gl.glTexCoord2f(1.0f, 0.0f);
+			gl.glVertex3f(1.0f, 1.0f, 1.0f);
+			gl.glTexCoord2f(1.0f, 1.0f);
+			gl.glVertex3f(1.0f, 1.0f, -1.0f);
+			// Bottom Face
+			gl.glTexCoord2f(1.0f, 1.0f);
+			gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+			gl.glTexCoord2f(0.0f, 1.0f);
+			gl.glVertex3f(1.0f, -1.0f, -1.0f);
+			gl.glTexCoord2f(0.0f, 0.0f);
+			gl.glVertex3f(1.0f, -1.0f, 1.0f);
+			gl.glTexCoord2f(1.0f, 0.0f);
+			gl.glVertex3f(-1.0f, -1.0f, 1.0f);
+			// Right face
+			gl.glTexCoord2f(1.0f, 0.0f);
+			gl.glVertex3f(1.0f, -1.0f, -1.0f);
+			gl.glTexCoord2f(1.0f, 1.0f);
+			gl.glVertex3f(1.0f, 1.0f, -1.0f);
+			gl.glTexCoord2f(0.0f, 1.0f);
+			gl.glVertex3f(1.0f, 1.0f, 1.0f);
+			gl.glTexCoord2f(0.0f, 0.0f);
+			gl.glVertex3f(1.0f, -1.0f, 1.0f);
+			// Left Face
+			gl.glTexCoord2f(0.0f, 0.0f);
+			gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+			gl.glTexCoord2f(1.0f, 0.0f);
+			gl.glVertex3f(-1.0f, -1.0f, 1.0f);
+			gl.glTexCoord2f(1.0f, 1.0f);
+			gl.glVertex3f(-1.0f, 1.0f, 1.0f);
+			gl.glTexCoord2f(0.0f, 1.0f);
+			gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+			gl.glEnd();
+
+			xrot += 0.3f;
+			yrot += 0.2f;
+			zrot += 0.4f;
+
+			myTexture.disable();
+		}
+
+		/**
+		 * Called when the display mode has been changed. <B>!! CURRENTLY
+		 * UNIMPLEMENTED IN JOGL !!</B>
+		 * 
+		 * @param gLDrawable
+		 *            The GLDrawable object.
+		 * @param modeChanged
+		 *            Indicates if the video mode has changed.
+		 * @param deviceChanged
+		 *            Indicates if the video device has changed.
+		 */
+		public void displayChanged(GLAutoDrawable gLDrawable,
+				boolean modeChanged, boolean deviceChanged) {
+		}
+
+		/**
+		 * Called by the drawable immediately after the OpenGL context is
+		 * initialized for the first time. Can be used to perform one-time
+		 * OpenGL initialization such as setup of lights and display lists.
+		 * 
+		 * @param gLDrawable
+		 *            The GLDrawable object.
+		 */
+		public void init(GLAutoDrawable gLDrawable) {
+			final GL gl = gLDrawable.getGL();
+			glu = new GLU(); // get GL Utilities
+
+			String resourceName = "nehe/lesson6/data/NeHe.png";
+			URL url = getResource(resourceName);
+			if (url == null) {
+				throw new RuntimeException("Error reading resource "
+						+ resourceName);
+			}
+			try {
+				myTexture = TextureIO.newTexture(url, false, ".png");
+			} catch (GLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			gl.glShadeModel(GL.GL_SMOOTH); // Enable Smooth Shading
+			gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f); // Black Background
+			gl.glClearDepth(1.0f); // Depth Buffer Setup
+			gl.glEnable(GL.GL_DEPTH_TEST); // Enables Depth Testing
+			gl.glDepthFunc(GL.GL_LEQUAL); // The Type Of Depth Testing To Do
+			gl.glHint(GL.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST); // Really
+																		// Nice
+																		// Perspective
+																		// Calculations
+			gl.glEnable(GL.GL_TEXTURE_2D);
+			gLDrawable.addKeyListener(this);
+			texture = genTexture(gl);
+			gl.glBindTexture(GL.GL_TEXTURE_2D, texture);
+			BufferedImage img = readPNGImage(resourceName);
+//			makeRGBTexture(gl, glu, img, GL.GL_TEXTURE_2D, false);
+			gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER,
+					GL.GL_LINEAR);
+			gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER,
+					GL.GL_LINEAR);
+		}
+
+		/**
+		 * Called by the drawable during the first repaint after the component
+		 * has been resized. The client can update the viewport and view volume
+		 * of the window appropriately, for example by a call to
+		 * GL.glViewport(int, int, int, int); note that for convenience the
+		 * component has already called GL.glViewport(int, int, int, int)(x, y,
+		 * width, height) when this method is called, so the client may not have
+		 * to do anything in this method.
+		 * 
+		 * @param gLDrawable
+		 *            The GLDrawable object.
+		 * @param x
+		 *            The X Coordinate of the viewport rectangle.
+		 * @param y
+		 *            The Y coordinate of the viewport rectanble.
+		 * @param width
+		 *            The new width of the window.
+		 * @param height
+		 *            The new height of the window.
+		 */
+		public void reshape(GLAutoDrawable gLDrawable, int x, int y, int width,
+				int height) {
+			final GL gl = gLDrawable.getGL();
+
+			if (height <= 0) // avoid a divide by zero error!
+				height = 1;
+			final float h = (float) width / (float) height;
+			gl.glViewport(0, 0, width, height);
+			gl.glMatrixMode(GL.GL_PROJECTION);
+			gl.glLoadIdentity();
+			glu.gluPerspective(45.0f, h, 1.0, 20.0);
+			gl.glMatrixMode(GL.GL_MODELVIEW);
+			gl.glLoadIdentity();
+		}
+
+		/**
+		 * Invoked when a key has been pressed. See the class description for
+		 * {@link KeyEvent} for a definition of a key pressed event.
+		 * 
+		 * @param e
+		 *            The KeyEvent.
+		 */
+		public void keyPressed(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+				animator.stop();
+				System.exit(0);
+			}
+		}
+
+		/**
+		 * Invoked when a key has been released. See the class description for
+		 * {@link KeyEvent} for a definition of a key released event.
+		 * 
+		 * @param e
+		 *            The KeyEvent.
+		 */
+		public void keyReleased(KeyEvent e) {
+		}
+
+		/**
+		 * Invoked when a key has been typed. See the class description for
+		 * {@link KeyEvent} for a definition of a key typed event.
+		 * 
+		 * @param e
+		 *            The KeyEvent.
+		 */
+		public void keyTyped(KeyEvent e) {
+		}
+
+		private BufferedImage readPNGImage(String resourceName) {
+			try {
+				URL url = getResource(resourceName);
+				if (url == null) {
+					throw new RuntimeException("Error reading resource "
+							+ resourceName);
+				}
+				BufferedImage img = ImageIO.read(url);
+				java.awt.geom.AffineTransform tx = java.awt.geom.AffineTransform
+						.getScaleInstance(1, -1);
+				tx.translate(0, -img.getHeight(null));
+				AffineTransformOp op = new AffineTransformOp(tx,
+						AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+				img = op.filter(img, null);
+				return img;
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		private void makeRGBTexture(GL gl, GLU glu, BufferedImage img,
+				int target, boolean mipmapped) {
+			ByteBuffer dest = null;
+			switch (img.getType()) {
+			case BufferedImage.TYPE_3BYTE_BGR:
+			case BufferedImage.TYPE_CUSTOM: {
+				System.out.println("or here");
+				byte[] data = ((DataBufferByte) img.getRaster().getDataBuffer())
+						.getData();
+				System.out.println("data.length = " + data.length);
+				dest = ByteBuffer.allocateDirect(data.length);
+				dest.order(ByteOrder.nativeOrder());
+				dest.put(data, 0, data.length);
+				break;
+			}
+			case BufferedImage.TYPE_INT_RGB: {
+				int[] data = ((DataBufferInt) img.getRaster().getDataBuffer())
+						.getData();
+				System.out.println("here");
+				dest = ByteBuffer.allocateDirect(data.length * 4);// BufferUtils.SIZEOF_INT);
+				dest.order(ByteOrder.nativeOrder());
+				dest.asIntBuffer().put(data, 0, data.length);
+				break;
+			}
+			default:
+				throw new RuntimeException("Unsupported image type "
+						+ img.getType());
+			}
+
+			if (mipmapped) {
+				glu.gluBuild2DMipmaps(target, GL.GL_RGB8, img.getWidth(),
+						img.getHeight(), GL.GL_RGB, GL.GL_UNSIGNED_BYTE, dest);
+			} else {
+				System.out.println("dest.capacity() = " + dest.capacity());
+				gl.glTexImage2D(target, 0, GL.GL_RGB, img.getWidth() / 2,
+						img.getHeight() / 2, 0, GL.GL_RGB, GL.GL_UNSIGNED_BYTE,
+						dest);
+			}
+		}
+
+		private int genTexture(GL gl) {
+			final int[] tmp = new int[1];
+			// gl.glGenTextures(1, tmp);
+			gl.glGenTextures(1, tmp, 0);
+			return tmp[0];
+		}
+	}
+
+	/**
+	 * Retrieve a URL resource from the jar. If the resource is not found, then
+	 * the local disk is also checked.
+	 * 
+	 * @param filename
+	 *            Complete filename, including parent path
+	 * @return a URL object if resource is found, otherwise null.
+	 */
+	public final static URL getResource(final String filename) {
+		// Try to load resource from jar
+		URL url = ClassLoader.getSystemResource(filename);
+		// If not found in jar, then load from disk
+		if (url == null) {
+			try {
+				url = new URL("file", "localhost", filename);
+			} catch (Exception urlException) {
+			} // ignore
+		}
+		return url;
+	}
+
+	/**
+	 * Program's main entry point
+	 * 
+	 * @param args
+	 *            command line arguments.
+	 */
+	public static void main(String[] args) {
+		Frame frame = new Frame("Lesson 6: Texture Mapping");
+		// Create the OpenGL rendering canvas
+		GLCanvas canvas = new GLCanvas(); // heavy-weight GLCanvas
+		canvas.addGLEventListener(new Renderer());
+		frame.add(canvas);
+		frame.setSize(640, 480);
+		animator = new Animator(canvas);
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				animator.stop();
+				System.exit(0);
+			}
+		});
+		frame.show();
+		animator.start();
+		canvas.requestFocus();
+	}
+}
